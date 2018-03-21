3452ed80f9ec81119e13a512dae99782bc7f6909
PMangels <pieter.mangelschots6@gmail.com>
2018-03-21 17:22:22 +0100

    Handling redirecting from / to /index.html and restructuring

--- src/http_server/Handler.java
+++ src/http_server/Handler.java
@@ -31,6 +31,112 @@ class Handler implements Runnable {
         this.socket = socket;
     }
 
+    //TODO: handle chunckes
+    @Override
+    public void run(){
+        try {
+            DataInputStream inFromClient = new DataInputStream(socket.getInputStream());
+            DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+            boolean shouldClose = false;
+            while (!shouldClose) {
+                Response response;
+                try {
+                    StringBuilder requestBuffer = new StringBuilder();
+
+                    while (!requestBuffer.toString().endsWith("\r\n\r\n")) {
+                        requestBuffer.append((char) inFromClient.readByte());
+                    }
+
+                    int length = parseRequestLength(requestBuffer.toString());
+
+                    int byteCount = 0;
+                    byte[] bytes = new byte[length];
+                    while (byteCount != length) {
+                        byteCount += inFromClient.read(bytes, byteCount, length - byteCount);
+                    }
+                    String byteString = new String(bytes,"UTF-8");
+                    requestBuffer.append(byteString);
+                    Request request = new Request(requestBuffer.toString());
+                    response = getResponse(request);
+                    if ("close".equals(request.getHeader("connection")) || request.getVersion() == HTTPVersion.HTTP10) {
+                        shouldClose = true;
+                    }
+                } catch (IllegalHeaderException e) {
+                    response = new Response(HTTPVersion.HTTP11, 400, "Bad Request", "Your HTTP request headers were malformed and could not be parsed. Error produced on line: " + e.getLine(), "text/plain");
+                } catch (IllegalRequestException e) {
+                    response = new Response(HTTPVersion.HTTP11, 400, "Bad Request", "Your request was not a valid HTTP request and could not be parsed.", "text/plain");
+                } catch (UnsupportedHTTPVersionException e) {
+                    response = new Response(HTTPVersion.HTTP11, 400, "Bad Request", "The provided HTTP version is not supported by this server.", "text/plain");
+                } catch (UnsupportedHTTPCommandException e) {
+                    response = new Response(HTTPVersion.HTTP11, 501, "Not Implemented");
+                }catch (SocketTimeoutException | SocketException | EOFException e){
+                    break;
+                }
+                catch (Throwable e) {
+                    response = new Response(HTTPVersion.HTTP11, 500, "Server Error", "An internal server error occurred while processing your request. Please try again.", "text/plain");
+                }
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.ENGLISH);
+                response.addHeader("Date", ZonedDateTime.now(ZoneId.of("GMT")).format(formatter));
+                System.out.println(response.toString());
+                outToClient.writeBytes(response.toString());
+            }
+
+            socket.close();
+
+        }catch (Throwable exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public Response getResponse(Request request) throws IOException, IllegalHeaderException {
+
+        if (request.getVersion() == HTTPVersion.HTTP11 && request.getHeader("host") == null)
+            return new Response(HTTPVersion.HTTP11, 400, "Bad Request", "HTTP 1.1 requests must include the Host: header", "text/plain");
+
+        String path = request.getPath();
+        if (path.startsWith("/"))
+            path = path.substring(1);
+
+        if (path.isEmpty() && HTTPVersion.HTTP11.equals(request.getVersion())){
+            Response response = new Response(request.getVersion(),303,"See Other");
+            response.addHeader("Location","/index.html");
+            return response;
+        }
+
+        String absolutePath = System.getProperty("user.dir") + "/public_html/" + path;
+        File f = new File(absolutePath);
+
+        switch (request.getType()){
+            case GET:
+                return fetchPage(request, f, false);
+            case POST:
+                if (f.isDirectory()) {
+                    return new Response(request.getVersion(), 400, "Bad Request", "The requested file could not be written to.", "text/plain");
+                }
+                String writingContent = request.getContent();
+                if(!f.createNewFile()){
+                    writingContent = "\r\n"+writingContent;
+                }
+                try (BufferedWriter output = new BufferedWriter(new FileWriter(absolutePath, true))) {
+                    output.append(writingContent);
+                }
+                return new Response(request.getVersion(), 200, "OK");
+            case PUT:
+                if (f.isDirectory()) {
+                    return new Response(request.getVersion(), 400, "Bad Request", "The requested file could not be written to.", "text/plain");
+                }
+                f.createNewFile();
+                try (BufferedWriter output = new BufferedWriter(new FileWriter(absolutePath, false))) {
+                    output.write(request.getContent());
+                }
+                return new Response(request.getVersion(), 200, "OK");
+            case HEAD:
+                return fetchPage(request, f, true);
+            default:
+                return new Response(HTTPVersion.HTTP11, 501, "Not Implemented");
+        }
+    }
+
     public Response fetchPage(Request request, File f, boolean headersOnly) throws IOException, IllegalHeaderException{
         if(f.exists() && !f.isDirectory()) {
             String dateString = request.getHeader("if-modified-since");
@@ -107,48 +213,6 @@ class Handler implements Runnable {
             return  "";
         }
     }
-    public Response getResponse(Request request) throws IOException, IllegalHeaderException {
-
-        if (request.getVersion() == HTTPVersion.HTTP11 && request.getHeader("host") == null)
-            return new Response(HTTPVersion.HTTP11, 400, "Bad Request", "HTTP 1.1 requests must include the Host: header", "text/plain");
-
-        String path = request.getPath();
-        if (path.startsWith("/"))
-            path = path.substring(1);
-
-        String absolutePath = System.getProperty("user.dir") + "/public_html/" + path;
-        File f = new File(absolutePath);
-
-        switch (request.getType()){
-            case GET:
-                return fetchPage(request, f, false);
-            case POST:
-                if (f.isDirectory()) {
-                    return new Response(request.getVersion(), 400, "Bad Request", "The requested file could not be written to.", "text/plain");
-                }
-                String writingContent = request.getContent();
-                if(!f.createNewFile()){
-                    writingContent = "\r\n"+writingContent;
-                }
-                try (BufferedWriter output = new BufferedWriter(new FileWriter(absolutePath, true))) {
-                    output.append(writingContent);
-                }
-                return new Response(request.getVersion(), 200, "OK");
-            case PUT:
-                if (f.isDirectory()) {
-                    return new Response(request.getVersion(), 400, "Bad Request", "The requested file could not be written to.", "text/plain");
-                }
-                f.createNewFile();
-                try (BufferedWriter output = new BufferedWriter(new FileWriter(absolutePath, false))) {
-                    output.write(request.getContent());
-                }
-                return new Response(request.getVersion(), 200, "OK");
-            case HEAD:
-                return fetchPage(request, f, true);
-            default:
-                return new Response(HTTPVersion.HTTP11, 501, "Not Implemented");
-        }
-    }
 
     private int parseRequestLength(String request) throws IllegalHeaderException {
         for (String line : request.split("\r\n")) {
@@ -166,60 +230,5 @@ class Handler implements Runnable {
         return 0;
     }
 
-    @Override
-    public void run(){
-        try {
-            DataInputStream inFromClient = new DataInputStream(socket.getInputStream());
-            DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
-            boolean shouldClose = false;
-            //TODO: check for a timeout
-            while (!shouldClose) {
-                Response response;
-                try {
-                    StringBuilder requestBuffer = new StringBuilder();
 
-                    while (!requestBuffer.toString().endsWith("\r\n\r\n")) {
-                        requestBuffer.append((char) inFromClient.readByte());
-                    }
-
-                    int length = parseRequestLength(requestBuffer.toString());
-
-                    int byteCount = 0;
-                    byte[] bytes = new byte[length];
-                    while (byteCount != length) {
-                        byteCount += inFromClient.read(bytes, byteCount, length - byteCount);
-                    }
-                    String byteString = new String(bytes,"UTF-8");
-                    requestBuffer.append(byteString);
-                    Request request = new Request(requestBuffer.toString());
-                    response = getResponse(request);
-                    if ("close".equals(request.getHeader("connection")) || request.getVersion() == HTTPVersion.HTTP10) {
-                        shouldClose = true;
-                    }
-                } catch (IllegalHeaderException e) {
-                    response = new Response(HTTPVersion.HTTP11, 400, "Bad Request", "Your HTTP request headers were malformed and could not be parsed. Error produced on line: " + e.getLine(), "text/plain");
-                } catch (IllegalRequestException e) {
-                    response = new Response(HTTPVersion.HTTP11, 400, "Bad Request", "Your request was not a valid HTTP request and could not be parsed.", "text/plain");
-                } catch (UnsupportedHTTPVersionException e) {
-                    response = new Response(HTTPVersion.HTTP11, 400, "Bad Request", "The provided HTTP version is not supported by this server.", "text/plain");
-                } catch (UnsupportedHTTPCommandException e) {
-                    response = new Response(HTTPVersion.HTTP11, 501, "Not Implemented");
-                }catch (SocketTimeoutException | SocketException | EOFException e){
-                    break;
-                }
-                catch (Throwable e) {
-                    response = new Response(HTTPVersion.HTTP11, 500, "Server Error", "An internal server error occurred while processing your request. Please try again.", "text/plain");
-                }
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.ENGLISH);
-                response.addHeader("Date", ZonedDateTime.now(ZoneId.of("GMT")).format(formatter));
-                System.out.println(response.toString());
-                outToClient.writeBytes(response.toString());
-            }
-
-            socket.close();
-
-        }catch (Throwable exception){
-            exception.printStackTrace();
-        }
-    }
 }
