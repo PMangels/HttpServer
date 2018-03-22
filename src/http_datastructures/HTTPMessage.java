fc983da3f27f5d3a40df8f1a13935c804339344c
Pieter Claeys <pieter.claeys@me.com>
2018-03-22 20:41:29 +0100

    Updated documentation for datastructures

--- src/http_datastructures/HTTPMessage.java
+++ src/http_datastructures/HTTPMessage.java
@@ -3,6 +3,10 @@ package http_datastructures;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a transaction in the HTTP protocol.
+ * Support for different versions, headers and contents.
+ */
 public class HTTPMessage {
 
     private String content = "";
@@ -19,19 +23,38 @@ public class HTTPMessage {
         this.terminationString = terminationString;
     }
 
+    /**
+     * Get the headers for this message.
+     * @return A map containing the headers for this message.
+     */
     public Map<String, String> getHeaders() {
         return headers;
     }
 
+    /**
+     * Get the contents of this message.
+     * @return A string containing the contents of the body this message
+     */
     public String getContent() {
         return content;
     }
 
-    public void addHeader(String key, String value){
+    /**
+     * Add a new header to this message.
+     * @param key A string containing the left side of this header.
+     * @param value A string containing the right side of this header.
+     */
+   public void addHeader(String key, String value){
         this.headers.put(key.toLowerCase(), value);
-    }
+   }
 
-    public String getHeader(String key){
+    /**
+     * Retrieves the value of the header with the provided key.
+     * @param key A string containing the left side of the requested header.
+     * @return A string containing the right hand side of the requested header,
+     *         or null if this header does not exist in this message.
+     */
+   public String getHeader(String key){
         return this.headers.get(key.toLowerCase());
     }
 
@@ -39,7 +62,12 @@ public class HTTPMessage {
         return this.headers.containsKey(key.toLowerCase());
     }
 
-    public String headerString(){
+    /**
+     * Returns the headers of this message as a string.
+     * @return The headers of this message, as a string. The format is as
+     *         specified in the HTTP standard
+     */
+   public String headerString(){
         StringBuilder output = new StringBuilder();
         for (Map.Entry<String, String> header : this.headers.entrySet()) {
             output.append(header.getKey());
@@ -50,20 +78,43 @@ public class HTTPMessage {
         return output.toString();
     }
 
+    /**
+     * Returns the version of the HTTP protocol being used for this message.
+     * @return The HTTPVersion used for this message.
+     */
     public HTTPVersion getVersion() {
         return version;
     }
 
+    /**
+     * Initialize a new HTTP message with the provided data,
+     * with the content-length header set to the length of the provided content
+     * and the content-type header set to the provided contentType string.
+     * @param version The HTTPVersion used for this message.
+     * @param content The body contents for this message.
+     * @param contentType The content-type header for this message.
+     */
     public HTTPMessage(HTTPVersion version, String content, String contentType){
         this(version);
         this.setContent(content, contentType);
     }
 
+    /**
+     * Initialize a new HTTP message with a provided version.
+     * No headers will be set and content will be null.
+     * @param version The HTTPVersion used for this message.
+     */
     public HTTPMessage(HTTPVersion version){
         this.version = version;
     }
 
-    public HTTPMessage(String rawMessage) throws IllegalHeaderException {
+    /**
+     * Initialize a new HTTP message by parsing it from the raw HTTP message string.
+     * The first line of the request will be saved as is, headers and content will be parsed and set,
+     * @param rawMessage The raw HTTP message to be parsed.
+     * @throws IllegalHeaderException When the headers in the raw message are malformed and could not be parsed.
+     */
+    HTTPMessage(String rawMessage) throws IllegalHeaderException {
         String[] parts = rawMessage.split("\r\n\r\n", 2);
         if (parts.length == 2){
             this.content = parts[1];
@@ -88,11 +139,22 @@ public class HTTPMessage {
         }
     }
 
+    /**
+     * Return this HTTP message as a string.
+     * @return This HTTP message as a HTTP transaction string.
+     */
     @Override
     public String toString() {
         return firstLine + "\r\n" + headerString() + "\r\n" + content + this.getTerminationString();
     }
 
+    /**
+     * Set the content of this message,
+     * with the content-length header set to the length of the provided content
+     * and the content-type header set to the provided contentType string.
+     * @param content The new contents of the body of this message.
+     * @param contentType The content-type of this body.
+     */
     public void setContent(String content, String contentType) {
         this.content = content;
         this.headers.put("content-length", String.valueOf(content.getBytes().length));
--- src/http_datastructures/Request.java
+++ src/http_datastructures/Request.java
@@ -5,14 +5,29 @@ public class Request extends HTTPMessage {
     private final RequestType type;
     private final String path;
 
+    /**
+     * Get the HTTP request type.
+     * @return The RequestType for this request.
+     */
     public RequestType getType() {
         return type;
     }
 
+    /**
+     * Get the path of this request.
+     * @return The path of the resource requested.
+     */
     public String getPath() {
         return path;
     }
 
+    /**
+     * Create a request with the provided data. Contents will be set to null,
+     * no headers will be set.
+     * @param type The HTTP request type.
+     * @param path The path for this request.
+     * @param version The HTTP version to be used with this request.
+     */
     public Request(RequestType type, String path, HTTPVersion version) {
         super(version);
 
@@ -21,8 +36,19 @@ public class Request extends HTTPMessage {
             path = "/" + path;
 
         this.path = path;
+        this.firstLine = type.toString()+ " " + path + " " + version.toString();
     }
 
+    /**
+     * Create a request with the provided data.
+     * Content-type headers will be set with the provided contentType string,
+     * Content-length headers will be set based on the provided contents length.
+     * @param type The HTTP request type.
+     * @param path The path for this request.
+     * @param version The HTTP version to be used with this request.
+     * @param content The contents for the body of this request.
+     * @param contentType The content-type header for this request.
+     */
     public Request(RequestType type, String path, HTTPVersion version, String content, String contentType) {
         super(version, content, contentType);
 
@@ -31,8 +57,17 @@ public class Request extends HTTPMessage {
             path = "/" + path;
 
         this.path = path;
+        this.firstLine = type.toString()+ " " + path + " " + version.toString();
     }
 
+    /**
+     * Create a new request by parsing it from a raw HTTP transaction string.
+     * @param requestString The raw HTTP request string.
+     * @throws UnsupportedHTTPCommandException When the provided HTTP request type is not supported.
+     * @throws UnsupportedHTTPVersionException When the provided HTTP version is not supported.
+     * @throws IllegalHeaderException When malformed headers were found.
+     * @throws IllegalRequestException When the request was malformed.
+     */
     public Request(String requestString) throws UnsupportedHTTPCommandException, UnsupportedHTTPVersionException, IllegalHeaderException, IllegalRequestException {
         super(requestString);
         String[] firstLine = this.firstLine.split(" ");
@@ -60,6 +95,10 @@ public class Request extends HTTPMessage {
 
     }
 
+    /**
+     * Returns this request as a HTTP request string.
+     * @return The HTTP request string for this request.
+     */
     @Override
     public String toString() {
         String terminationString = "\r\n\r\n";
